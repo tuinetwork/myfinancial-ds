253078af5755fa71c1fb56459ed199063d87994c
Tui <t.tuinmd@gmail.com>
2026-03-14 07:39:05 +0700

    Update firebase configuration and fix login issue

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
@@ -12,6 +12,15 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDV_vAmTI_VAdJMHnZG-KFH7lS1XfTmeds", // ก๊อปปี้มาจาก Firebase Console
+  authDomain: "myfinancedashboard-5e96a.firebaseapp.com",
+  projectId: "myfinancedashboard-5e96a",
+  storageBucket: "myfinancedashboard-5e96a.appspot.com",
+  messagingSenderId: "825108223515",
+  appId: "1:825108223515:web:7c9867bd501741df240abe"
 };
 
 const app = initializeApp(firebaseConfig);
